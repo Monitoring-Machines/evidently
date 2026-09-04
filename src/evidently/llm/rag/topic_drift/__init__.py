98488fc4fd8658f5e1ac3cbeddb165b110c77a80
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-09-04 17:37:12 +0200

    Update __init__.py

--- src/evidently/llm/rag/topic_drift/__init__.py
+++ src/evidently/llm/rag/topic_drift/__init__.py
@@ -1,2 +1,2 @@
 from src.evidently.llm.rag.topic_drift.topic_content_query_drift import TopicContentQueryDrift
-
+from src.evidently.llm.rag.topic_drift.topic_semantic_drift import TopicSemanticContentDrift
